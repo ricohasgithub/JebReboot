2f3a6460184d329514390b975dc0340741e94439
ricozhuthegreat <33135503+ricozhuthegreat@users.noreply.github.com>
2019-05-02 16:02:56 -0400

    Created RobotMap.java
     - Added the RobotMap.java class that maps the different motorcontrollers and pneumatics to their respective ports

---
+++ JebReboot/src/main/java/frc/robot/RobotMap.java
@@ -0,0 +1,18 @@
+
+package frc.robot;
+
+public class RobotMap {
+
+    // **************************** DRIVE *********************************
+    public static final int Left1 = 0;
+    public static final int Left2 = 1;
+    public static final int Right1 = 2;
+    public static final int Right2 = 3;
+
+    // **************************** ARM *********************************
+    public static final int Arm = 4;
+
+    // **************************** SHOOTER *********************************
+    public static final int Shooter = 5;
+
+}
