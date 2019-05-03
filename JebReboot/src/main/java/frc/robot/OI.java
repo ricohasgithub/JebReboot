775d8d32519ab9679bbb152208ed9cd9ff63e84c
ricozhuthegreat <33135503+ricozhuthegreat@users.noreply.github.com>
2019-05-03 08:18:38 -0400

    Created OI.java
     - Added basic drive controls in OI.java

---
+++ JebReboot/src/main/java/frc/robot/OI.java
@@ -0,0 +1,20 @@
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
+
+public class OI {
+
+    public static XboxController Driver = new XboxController(0);
+    public static XboxController Operator = new XboxController(1);
+
+    public static double getDriverSpeed () {
+        return Driver.getTriggerAxis(Hand.kRight) - Driver.getTriggerAxis(Hand.kRight);
+    }
+
+    public static double getDriverTurn () {
+        return Driver.getRawAxis(0);
+    }
+
+}
